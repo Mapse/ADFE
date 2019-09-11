e70c00b594798e6cd6565029722e4b36013031ce
Mapse <mapse.b@hotmail.com>
2019-09-11 09:03:00 -0300

    Inserção da função crystalball

--- Fits.C
+++ Fits.C
@@ -14,3 +14,15 @@ void gausian(){
     ga->SetParameter(2, 1.2);
     ga->Draw();
 }
+
+void crystb(){
+    // Cria a crystalball com ranges de -5 a 5.
+    TF1 * cr = new TF1("cr", "crystalball", -5, 5);
+    // Define os parâmetros: o primeiro é o ponto máximo da função(ymax), 
+    //o segundo é o ponto central (ymax=f(xcent)),
+    //o terceiro é a largura (ver direito ainda),
+    //o quarto é a altura da calda inicial
+    // o quinto eu não sei
+    cr->SetParameters(1, 0, 1, 2, 5); 
+    cr->Draw();
+}
